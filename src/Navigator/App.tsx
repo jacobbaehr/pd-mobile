bf8ddcf50d48f88081a4ff6e647ca7338bb916de
Wade Cosby <wadecosby@wades-mbp.attlocal.net>
2017-12-20 20:59:03 -0600

    Changed referenced HomeScreen to ReadingList screen EVERYWHERE

--- src/Navigator/App.tsx
+++ src/Navigator/App.tsx
@@ -1,12 +1,12 @@
 import { StackNavigator } from 'react-navigation';
-import { HomeScreen } from '../screens/HomeScreen';
+import { ReadingListScreen } from '../screens/ReadingListScreen';
 import { DetailsScreen } from '../screens/DetailsScreen';
 import { ResultsScreen } from '../screens/ResultsScreen';
 import { CalculationSettingsScreen } from '../screens/CalculationSettingsScreen';
 import { PoolScreen } from '../screens/PoolScreen';
 
 export const App = StackNavigator({
-  Home: { screen: HomeScreen },
+  Home: { screen: ReadingListScreen },
   Details: { screen: DetailsScreen },
   Results: { screen: ResultsScreen },
   Settings: { screen: CalculationSettingsScreen },
