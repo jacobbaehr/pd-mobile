e01e7b172db5aaa3ed2461334e0d402fda467bea
gazzini <gazzini.john@gmail.com>
2020-06-23 01:11:24 -0500

    updates life story, points prod at dev

--- src/screens/buy/LifeStory.ts
+++ src/screens/buy/LifeStory.ts
@@ -1,2 +1,2 @@
 export const lifeStory = `
-I used to maintain pools for a living, and now I’m a software engineer at a big company. This is my passion-project, and I hope it makes your day a little better :)`;
+I used to maintain pools for a living. Now, I’m a software engineer at a big company, and this is my passion-project. I hope it makes your day a little better!`;
