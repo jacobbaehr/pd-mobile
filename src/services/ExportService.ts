2ccb6d12d095bd3260a468e7f897917c153cfa7b
gazzini <gazzini.john@gmail.com>
2021-01-05 23:35:45 -0600

    updates csv export on android

--- src/services/ExportService.ts
+++ src/services/ExportService.ts
@@ -1,19 +1,18 @@
 import Share, { Options, MultipleOptions } from 'react-native-share';
 import { Config } from './Config';
-import { Util } from './Util';
 
 export namespace ExportService {
 
     export const shareCSVFile = async (fileData: string): Promise<void> => {
         return new Promise((resolve, reject) => {
-            const fileName = `pd-${(new Date()).toISOString()}.csv`;
+            const fileName = `pd-${(new Date()).toISOString()}`;
             const shareOptions: Options | MultipleOptions = {};
             if (Config.isAndroid) {
                 const sharableURL = `data:text/comma-separated-values;base64,${fileData}`;
                 console.log(sharableURL);
                 shareOptions.url = sharableURL;
                 shareOptions.type = 'text/csv';
-                shareOptions.filename = Util.stringToBase64(fileName);
+                shareOptions.filename = fileName;
             } else {
                 const sharableURL = `data:text/comma-separated-values;base64,${fileData}`;
                 console.log(sharableURL);
@@ -26,10 +25,10 @@ export namespace ExportService {
                             },
                         },
                         dataTypeIdentifier: { default: 'kUTTypeCommaSeparatedText' },
-                        subject: { default: fileName },
+                        subject: { default: `${fileName}.csv` },
                         linkMetadata: {
                             title: 'pooldash export',
-                            originalUrl: fileName
+                            originalUrl: `${fileName}.csv`
                         }
                     }
                 ];
