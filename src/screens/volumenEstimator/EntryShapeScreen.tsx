2fee60bbc979a7591506245f44742bf92068e235
Sebastian Perez <sperezm97@gmail.com>
2021-03-03 13:00:44 -0400

    PD-68: Create empty SelectShapeScreen and EntryShapeShape

---
+++ src/screens/volumenEstimator/EntryShapeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const EntryShapeScreen = () => {
+    return (
+        <View>
+            <Text />
+        </View>
+    );
+};
+
+export default EntryShapeScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/volumenEstimator/SelectShapeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SelectShapeScreen = () => {
+    return (
+        <View>
+            <Text />
+        </View>
+    );
+};
+
+export default SelectShapeScreen;
+
+const styles = StyleSheet.create({});
