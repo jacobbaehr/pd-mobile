d08c2d43753451c0c5e68f6d2c0a6832d785620f
Sebastian Perez <sperezm97@gmail.com>
2021-03-04 08:24:15 -0400

    PD-68: Create VolumenNavigator

--- src/navigator/shared.ts
+++ src/navigator/shared.ts
@@ -1,4 +1,9 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+
 import { PDCardNavigatorParams } from './PDCardNavigator';
 import { PDRootNavigatorParams } from './PDRootNavigator';
+import { PDVolumesParams } from './PDVolumesNavigator';
 
-export type PDNavParams = PDCardNavigatorParams & PDRootNavigatorParams;
+export type PDNavParams = PDCardNavigatorParams & PDRootNavigatorParams & PDVolumesParams;
+
+export type PDStackNavigationProps = StackNavigationProp<PDNavParams>;
