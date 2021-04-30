f384e34103563c09741ba490fffa1aa1c87c5fbf
gazzini <gazzini.john@gmail.com>
2021-04-29 22:45:50 -0500

    reverts purchases hack -- whoops, that was close

--- src/services/DeviceSettingsService.ts
+++ src/services/DeviceSettingsService.ts
@@ -17,40 +17,40 @@ export class DeviceSettingsService {
         };
     };
 
-    // static getSettings = async (): Promise<DeviceSettings> => {
-    //     const asString = await AsyncStorage.getItem(DEVICE_SETTINGS_KEY);
-    //     if (!asString) {
-    //         return DeviceSettingsService.getDefaultSettings();
-    //     }
-    //     const rs = JSON.parse(asString) as RawDeviceSettings;
-
-    //     // fill it in (if necessary)
-    //     const ds = DeviceSettingsService.rawDeviceSettingsToDeviceSettings(rs);
-
-    //     return ds;
-    // };
-
-    /************************
-     * This will "trick" your local app into thinking that the app is unlocked,
-     * but won't allow you to actually test the checkout flow in dev.
-     * *********************/
     static getSettings = async (): Promise<DeviceSettings> => {
-        console.warn('USING TEST PURCHASE OVERRIDE, REVERT BEFORE MERGING');
         const asString = await AsyncStorage.getItem(DEVICE_SETTINGS_KEY);
         if (!asString) {
-            const ds = DeviceSettingsService.getDefaultSettings();
-            ds.sub_exp = (new Date()).getTime() + 30000000;
-            return ds;
+            return DeviceSettingsService.getDefaultSettings();
         }
         const rs = JSON.parse(asString) as RawDeviceSettings;
 
         // fill it in (if necessary)
         const ds = DeviceSettingsService.rawDeviceSettingsToDeviceSettings(rs);
 
-        ds.sub_exp = (new Date()).getTime() + 30000000;
-
         return ds;
-    }
+    };
+
+    /************************
+     * This will "trick" your local app into thinking that the app is unlocked,
+     * but won't allow you to actually test the checkout flow in dev.
+     * *********************/
+    // static getSettings = async (): Promise<DeviceSettings> => {
+    //     console.warn('USING TEST PURCHASE OVERRIDE, REVERT BEFORE MERGING');
+    //     const asString = await AsyncStorage.getItem(DEVICE_SETTINGS_KEY);
+    //     if (!asString) {
+    //         const ds = DeviceSettingsService.getDefaultSettings();
+    //         ds.sub_exp = (new Date()).getTime() + 30000000;
+    //         return ds;
+    //     }
+    //     const rs = JSON.parse(asString) as RawDeviceSettings;
+
+    //     // fill it in (if necessary)
+    //     const ds = DeviceSettingsService.rawDeviceSettingsToDeviceSettings(rs);
+
+    //     ds.sub_exp = (new Date()).getTime() + 30000000;
+
+    //     return ds;
+    // }
 
     static saveSettings = async (settings: DeviceSettings): Promise<void> => {
         const asString = JSON.stringify(settings);
